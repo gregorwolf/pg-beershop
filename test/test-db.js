9a45021464c2409362f089d3e5cf55bf6e269891
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-16 17:00:09 +0200

    Fix direct db connection test

--- test/test-db.js
+++ test/test-db.js
@@ -4,7 +4,7 @@ const { Client } = require('pg')
 const client = new Client(defaultEnv.VCAP_SERVICES.postgres[0].credentials)
 ;(async () => {
   await client.connect()
-  const res = await client.query('SELECT "ID", "name" FROM "BeershopService_Beers" AS "Beers";')
+  const res = await client.query('SELECT ID AS "ID", name AS "name" FROM BeershopService_Beers AS "Beers";')
   console.log(res.rows[0])
   await client.end()
 })()
