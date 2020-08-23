6170a8aa19b64d155298f6f0255d72a5e6b5c3a3
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-23 20:20:48 +0200

    Adjustments for pg on Azure

---
+++ beershop-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO "csw_beers" ("id", "name", "abv", "ibu", "brewery_id") VALUES
+('b8c3fc14-22e2-4f42-837a-e6134775a186',	'Lagerbier Hell',	5.2,	12,	'9c937100-d459-491f-a72d-81b2929af10f'),
+('9e1704e3-6fd0-4a5d-bfb1-13ac47f7976b',	'Schönramer Hell',	5.0,	20,	'fa6b959e-3a01-40ef-872e-6030ee4de4e5');
+
+INSERT INTO "csw_brewery" ("id", "name") VALUES
+('9c937100-d459-491f-a72d-81b2929af10f',	'Augustiner-Bräu Wagner KG'),
+('fa6b959e-3a01-40ef-872e-6030ee4de4e5',	'Private Landbrauerei Schönram GmbH & Co. KG');
