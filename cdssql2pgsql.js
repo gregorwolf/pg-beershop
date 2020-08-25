e8156d5b23255a7c3c36c5a1caeb23e6f0e04d2f
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-25 22:03:07 +0200

    Use cds-pg for cdssql2pgsql

--- cdssql2pgsql.js
+++ cdssql2pgsql.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
+const postgresDatabase = require('cds-pg')
+
+const cdspg = new postgresDatabase()
 
 const cdssql = fs.readFileSync('beershop-cds.sql').toString('utf-8')
-var pgsql = cdssql.replace(/NVARCHAR/g, 'VARCHAR')
-pgsql += "\nCOPY csw_Beers(ID, name, abv, ibu) " 
+let pgsql = cdspg.cdssql2pgsql(cdssql)
+pgsql += "\nCOPY csw_Beers(ID, name, abv, ibu, brewery_ID) " 
 + "FROM '/tmp/data/csw-Beers.csv' "
 + "DELIMITER ',' CSV HEADER;"
 pgsql += "\nCOPY csw_Brewery(ID, name) " 
