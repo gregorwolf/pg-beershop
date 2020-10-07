94024c2686c9082f5ccea007729ebb72150d87a6
Gregor Wolf <gregor.wolf@gmail.com>
2020-10-07 22:01:13 +0200

    remove appinsights

--- srv/server.js
+++ srv/server.js
@@ -1,8 +1,10 @@
 const proxy = require('@sap/cds-odata-v2-adapter-proxy')
 const cds = require('@sap/cds')
+/*
 if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
   let appInsights = require('applicationinsights')
   appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start()
 }
+*/
 cds.on('bootstrap', app => app.use(proxy()))
 module.exports = cds.server
