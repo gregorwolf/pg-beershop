b08e20ba5d84e1961a9df00edf735004e7db0268
Gregor Wolf <gregor.wolf@gmail.com>
2020-10-07 21:37:38 +0200

    Only start appinsights when env variable set

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,8 @@
 const proxy = require('@sap/cds-odata-v2-adapter-proxy')
 const cds = require('@sap/cds')
-let appInsights = require('applicationinsights')
-appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start()
+if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+  let appInsights = require('applicationinsights')
+  appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start()
+}
 cds.on('bootstrap', app => app.use(proxy()))
 module.exports = cds.server
