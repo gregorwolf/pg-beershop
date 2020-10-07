6bedf1f23697099ff98b051cb5c8f08fdd98f114
Gregor Wolf <gregor.wolf@gmail.com>
2020-10-07 21:31:21 +0200

    Add applicationinsights

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,6 @@
 const proxy = require('@sap/cds-odata-v2-adapter-proxy')
 const cds = require('@sap/cds')
+let appInsights = require('applicationinsights')
+appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start()
 cds.on('bootstrap', app => app.use(proxy()))
 module.exports = cds.server
