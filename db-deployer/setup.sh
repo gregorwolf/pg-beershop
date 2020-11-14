e929317acc2e985df568cb73aaa7556bf45d38f7
Gregor Wolf <gregor.wolf@gmail.com>
2020-11-15 00:24:13 +0100

    make certificate useable for liquibase

--- db-deployer/setup.sh
+++ db-deployer/setup.sh
@@ -3,7 +3,10 @@
 export VCAP_SERVICES=`echo $VCAP_SERVICES | jq '."user-provided"[0].tags[0] |= .+ "database"'`
 # Save Certificate from Environment where liquibase expects it
 mkdir -p /home/vcap/.postgresql
-echo $VCAP_SERVICES | jq --raw-output '."user-provided"[0].credentials.sslrootcert' > /home/vcap/.postgresql/root.crt
+export POSTGRESQL_ROOT_CERT="/home/vcap/.postgresql/root.crt"
+echo $VCAP_SERVICES | jq --raw-output '."user-provided"[0].credentials.sslrootcert' > $POSTGRESQL_ROOT_CERT
+sed -i 's/BEGIN CERTIFICATE-----/BEGIN CERTIFICATE-----\n/g' $POSTGRESQL_ROOT_CERT
+sed -i 's/-----END/\n-----END/g' $POSTGRESQL_ROOT_CERT
 # Install SAP Machine
 cd ..
 wget https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.9.1/sapmachine-jdk-11.0.9.1_linux-x64_bin.tar.gz
