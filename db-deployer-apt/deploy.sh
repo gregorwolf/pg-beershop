9405897e4ce63cb8bd3749fc5fe9c3abce1b7a61
Gregor Wolf <gregor.wolf@gmail.com>
2020-11-19 11:25:25 +0100

    Working liquibase with openjdk-8

--- db-deployer-apt/deploy.sh
+++ db-deployer-apt/deploy.sh
@@ -2,7 +2,8 @@
 set -x #echo on
 # Add tag database to VCAP_SERVICES - not needed when user provided service is created with cf command line
 # export VCAP_SERVICES=`echo $VCAP_SERVICES | jq '."user-provided"[0].tags[0] |= .+ "database"'`
-export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/java-11-openjdk-amd64
+# export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/java-11-openjdk-amd64
+export JAVA_HOME=/home/vcap/deps/0/apt/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$PATH:/home/vcap/deps/1/bin
 # Until we use the node package for cds-dbm we need to install the dev dependencies
 export NODE_ENV=development
