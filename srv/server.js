33099275c18f50efe9847e829791833ae8527ba4
Gregor Wolf <gregor.wolf@gmail.com>
2022-07-14 13:51:53 +0200

    feat: add cds-launchpad-plugin

--- srv/server.js
+++ srv/server.js
@@ -1,10 +1,21 @@
-const proxy = require('@sap/cds-odata-v2-adapter-proxy')
-const cds = require('@sap/cds')
+const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const cds = require("@sap/cds");
 /*
 if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
   let appInsights = require('applicationinsights')
   appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start()
 }
 */
-cds.on('bootstrap', app => app.use(proxy()))
-module.exports = cds.server
+
+if (process.env.NODE_ENV !== "production") {
+  const { cds_launchpad_plugin } = require("cds-launchpad-plugin");
+
+  // Enable launchpad plugin
+  cds.once("bootstrap", (app) => {
+    const handler = new cds_launchpad_plugin();
+    app.use(handler.setup());
+  });
+}
+
+cds.on("bootstrap", (app) => app.use(proxy()));
+module.exports = cds.server;
