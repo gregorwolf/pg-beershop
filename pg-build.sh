891f7c10184b75aae048095ebd40fc64dbadde03
Gregor Wolf <gregor.wolf@gmail.com>
2023-07-09 20:09:29 +0200

    do not copy default data

--- pg-build.sh
+++ pg-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p gen/pg/srv
-cp -r db/data gen/pg/srv
+# Works only the first time until https://github.com/cap-js/cds-dbs/issues/100 is fixed
+# cp -r db/data gen/pg/srv
 cds compile '*' > gen/pg/srv/csn.json
 cp pg-package.json gen/pg/package.json
+cp package-lock.json gen/pg/package-lock.json
