e3f74e5aff71aaa752c4e698ef9f2ee5968656c8
Gregor Wolf <gregor.wolf@gmail.com>
2023-07-14 16:28:52 +0200

    chore: add mbt as a dev dependency update pg-build.sh to follow documentation

--- pg-build.sh
+++ pg-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir -p gen/pg/srv
+mkdir -p gen/pg/db
 # Works only the first time until https://github.com/cap-js/cds-dbs/issues/100 is fixed
-# cp -r db/data gen/pg/srv
-cds compile '*' > gen/pg/srv/csn.json
+# cp -r db/data gen/pg/db
+cds compile '*' > gen/pg/db/csn.json
 cp pg-package.json gen/pg/package.json
 cp package-lock.json gen/pg/package-lock.json
